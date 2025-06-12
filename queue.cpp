e7346f342343e400e7f9ad70a47039deb35c28e3
i-i-ijul <haqqikamvret@gmail.com>
2025-06-12 10:59:44 +0700

    memberikan pilihan case

--- queue.cpp
+++ queue.cpp
@@ -64,8 +64,8 @@ public:
         }
         else
         {
-            //jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
-            if (front == max -1)
+            // jika element yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (front == max - 1)
                 front = 0;
             else
                 front = front + 1;
@@ -77,7 +77,7 @@ public:
         int front_position = front;
         int rear_position = rear;
 
-        //cek apakah antrian kosong
+        // cek apakah antrian kosong
         if (front == -1)
         {
             cout << "queue is empty\n";
@@ -86,7 +86,7 @@ public:
 
         cout << "\nelements in the queue are...\n";
 
-        //jika front position <= rear_position, iterasi dari front hingga rear
+        // jika front position <= rear_position, iterasi dari front hingga rear
         if (front_position <= rear_position)
         {
             while (front_position <= rear_position)
@@ -98,8 +98,8 @@ public:
         }
         else
         {
-            //jika front_position > rear_position, iterasi dari front hingga akhir array
-            while (front_position <= max -1)
+            // jika front_position > rear_position, iterasi dari front hingga akhir array
+            while (front_position <= max - 1)
             {
                 cout << queue_array[front_position] << " ";
                 front_position++;
@@ -107,7 +107,7 @@ public:
 
             front_position = 0;
 
-            //iterasi dari awal array hingga rear
+            // iterasi dari awal array hingga rear
             while (front_position <= rear_position)
             {
                 cout << queue_array[front_position] << " ";
@@ -115,7 +115,6 @@ public:
             }
             cout << endl;
         }
-
     }
 };
 
@@ -124,7 +123,7 @@ int main()
     Queues q;
     char ch;
 
-    while(true)
+    while (true)
     {
         try
         {
@@ -139,11 +138,35 @@ int main()
 
             switch (ch)
             {
-                case '1':
-                {
-                    
-                }
+            case '1':
+            {
+                q.insert();
+                break;
             }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "invalid option!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
         }
     }
 }
