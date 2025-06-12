30a9e7b90694048b5c1f75c43ac86691144e104a
i-i-ijul <haqqikamvret@gmail.com>
2025-06-12 10:08:54 +0700

    menambahkan class queues

--- queue.cpp
+++ queue.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Queues
+{
+    private:
+    static const int max = 5;
+    int front, rear;
+    int queue_array[5];
+
+    public:
+    Queues()
+    {
+        front = -1;
+        rear = -1;
+    }
+
+    void insert()
+    {
+        
+    }
+
+};
 int main(){
 
 } 
